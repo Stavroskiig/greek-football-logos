bdf9d8b9c821cb61ca12889aff02e0574c9d8d2e
Stavros Kiigmas <stavroskiig@protonmail.com>
2026-03-03 20:31:19 +0200

    Improve seo

--- frontend/generate-logos-manifest.js
+++ frontend/generate-logos-manifest.js
@@ -18,7 +18,7 @@ function scanLogos(dir, league = '') {
       const teamName = path.basename(item.name, path.extname(item.name))
         .replace(/-/g, ' ')
         .replace(/\b\w/g, l => l.toUpperCase());
-      
+
       result.push({
         name: teamName,
         path: `assets/logos/${league ? league + '/' : ''}${item.name}`,
@@ -33,3 +33,52 @@ function scanLogos(dir, league = '') {
 const logos = scanLogos(logosDir);
 fs.writeFileSync(outputFile, JSON.stringify(logos, null, 2));
 console.log(`Generated manifest with ${logos.length} logos at ${outputFile}`);
+
+// Generate sitemap.xml with Image extensions
+const sitemapFile = path.join(__dirname, 'src', 'sitemap.xml');
+const baseUrl = 'https://greek-football-logos.site';
+const currentDate = new Date().toISOString().split('T')[0];
+
+let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+`;
+
+logos.forEach(logo => {
+  const safeTitle = logo.name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  sitemapXml += `    <image:image>
+      <image:loc>${baseUrl}/${encodeURI(logo.path)}</image:loc>
+      <image:title>${safeTitle} Logo / Λογότυπο</image:title>
+    </image:image>
+`;
+});
+
+sitemapXml += `  </url>
+  <url>
+    <loc>${baseUrl}/misc</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/tag-manager</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/collections</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>
+`;
+
+fs.writeFileSync(sitemapFile, sitemapXml);
+console.log(`Generated sitemap with ${logos.length} images at ${sitemapFile}`);
