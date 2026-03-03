94ca933c99977b73e39cabe900054717c4a2c274
Stavros Kiigmas <stavroskiig@protonmail.com>
2026-03-03 20:09:28 +0200

    Fix health

--- backend/src/main/java/com/gfl/api/dto/LogoSyncDTO.java
+++ backend/src/main/java/com/gfl/api/dto/LogoSyncDTO.java
@@ -1,8 +1,10 @@
 package com.gfl.api.dto;
 
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LogoSyncDTO {
     private String name;
     private String path;
